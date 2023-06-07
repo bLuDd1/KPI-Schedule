48adc343c3ab90ced1db6247c89140174ecdc321
bLuDd1 <dtavluj@gmail.com>
2023-06-07 20:27:15 +0300

    Created variables for schedule

--- KPI-Schedule/Controller/ScheduleController.swift
+++ KPI-Schedule/Controller/ScheduleController.swift
@@ -1,23 +1,20 @@
-//
-//  ScheduleController.swift
-//  KPI-Schedule
-//
-//  Created by Dima Tavlui on 05.06.2023.
-//
-
 import UIKit
 
 class ScheduleController: UITableViewController {
+    
+    var scheduleManager = ScheduleManager()
+    var firstWeekSchedule = [Day]()
+    var secondWeekSchedule = [Day]()
+    var scheduleWeek = [Day]()
+    var defaults = UserDefaults()
+    var daysWithPairs = [Day]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
 
 }
+
