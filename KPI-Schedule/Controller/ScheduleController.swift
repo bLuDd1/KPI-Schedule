ce3b6687c0a8437797dd1f96dbcbe8fba97efd99
bLuDd1 <dtavluj@gmail.com>
2023-06-08 01:56:49 +0300

    Added extension

--- KPI-Schedule/Controller/ScheduleController.swift
+++ KPI-Schedule/Controller/ScheduleController.swift
@@ -8,24 +8,48 @@ class ScheduleController: UITableViewController {
     var scheduleWeek = [Day]()
     var defaults = UserDefaults()
     var daysWithPairs = [Day]()
+    let defaultId = "4700e2be-e8a9-4b9e-859e-b44ece843445"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        scheduleManager.delegate = self
+        navigationItem.title = defaults.string(forKey: "selectedGroupName") ?? "ІМ-13"
+        scheduleManager.getSchedule(id: defaults.string(forKey: "selectedGroupId") ?? defaultId)
 
     }
     
     @IBAction func changeWeek(_ sender: UISegmentedControl) {
-    let selectedSchedule = sender.selectedSegmentIndex == 1 ? secondWeekSchedule : firstWeekSchedule
-    scheduleWeek = selectedSchedule
+        let selectedSchedule = sender.selectedSegmentIndex == 1 ? secondWeekSchedule : firstWeekSchedule
+        scheduleWeek = selectedSchedule
     
-    DispatchQueue.main.async {
+        DispatchQueue.main.async {
         self.tableView.reloadData()
+        }
+}
+
+}
+
+extension ScheduleController: ScheduleManagerDelegate {
+    func didUpdate(_ scheduleManager: ScheduleManager, schedule: ScheduleData) {
+        firstWeekSchedule = sortPairs(in: schedule.data.scheduleFirstWeek)
+        secondWeekSchedule = sortPairs(in: schedule.data.scheduleSecondWeek)
+        scheduleWeek = firstWeekSchedule
+
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    func didFail(error: Error) {
+        print(error)
+    }
+    
+    func sortPairs(in week: [Day]) -> [Day] {
+        var newWeek = week
+        for i in 0...5 {
+            //TODO: don't use !
+            newWeek[i].pair.sort { Double($0.time)! < Double($1.time)! }
+        }
+        return newWeek
     }
 }
-
-    
-    
-
-}
-
